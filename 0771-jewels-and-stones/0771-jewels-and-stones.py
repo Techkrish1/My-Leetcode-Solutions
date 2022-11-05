0d3e5ceaa62ef67a8fff2bd0b7dae909c3226fc0
Gokula krishnan Venkatesan <111285228+Techkrish1@users.noreply.github.com>
2022-11-05 17:53:09 +0530

    Time: 58 ms (50.32%), Space: 13.8 MB (96.75%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,8 +1,10 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        counter = 0
-        jewels = set(jewels) # search in a set is instant - O(1)
-        for stone in stones:
-            if stone in jewels:
-                counter += 1
-        return counter
+        d_jewels = dict()
+        for i in jewels:
+            d_jewels[i] = d_jewels.get(i,0) + 1
+        old_sum = sum(d_jewels.values())
+        for i in stones:
+            if i in d_jewels:
+                d_jewels[i] = d_jewels.get(i,0) + 1
+        return sum(d_jewels.values()) - old_sum
