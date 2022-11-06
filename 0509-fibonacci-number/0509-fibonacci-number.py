26ae5661860ab983bf7b6ffebc175f7dc2745333
Gokula krishnan Venkatesan <111285228+Techkrish1@users.noreply.github.com>
2022-11-06 21:17:12 +0530

    Time: 2006 ms (11.17%), Space: 13.8 MB (96.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,14 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
-        left  = 0
-        right = 1
         if n == 0:
             return 0
         elif n == 1:
             return 1
         else:
-            for i in range(2,n+1):
-                temp = right
-                right = left + right
-                left = temp
-            return right
+            return self.fib(n-1) + self.fib(n-2)
