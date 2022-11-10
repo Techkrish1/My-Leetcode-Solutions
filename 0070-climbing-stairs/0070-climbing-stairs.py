84accb5c8334c7e485b1227fe8888531059cbdcc
Gokula krishnan Venkatesan <111285228+Techkrish1@users.noreply.github.com>
2022-11-10 21:58:47 +0530

    Time: 67 ms (9.31%), Space: 13.8 MB (57.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,7 @@
 class Solution:
+    def __init__(self):
+        self.dic = {1:1, 2:2}
     def climbStairs(self, n: int) -> int:
-        one , two = 1 , 1
-        for i in range(n-1):
-            temp = one
-            one = one + two
-            two = temp
-        return one
+        if n not in self.dic:
+            self.dic[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.dic[n]
