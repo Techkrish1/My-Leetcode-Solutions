d90d34650019a500eaebaf0c8f7b50ecca132457
Gokula krishnan Venkatesan <111285228+Techkrish1@users.noreply.github.com>
2023-01-20 15:52:30 +0530

    Time: 225 ms (80.99%), Space: 25.4 MB (77.01%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,6 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(k):
-            pop = nums.pop()
-            nums.insert(0,pop)
+             
+        def twopt(arr, i, j):
+            while (i < j):
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
+                j -= 1
+            return arr
+        
+        if k > len(nums):
+            k %= len(nums)
+            
+        if (k > 0):
+            twopt(nums, 0, len(nums) - 1)  
+            twopt(nums, 0, k - 1)          
+            twopt(nums, k, len(nums) - 1)  
