c348c266910a7b51708ed827a45a253644743cfd
Gokula krishnan Venkatesan <111285228+Techkrish1@users.noreply.github.com>
2023-03-09 21:24:13 +0530

    Time: 46 ms (36.57%), Space: 14.1 MB (50.76%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) <= 1:
+            return [nums]
+
+        ans = []
+        queue = deque([([], nums)])
+
+        while queue:
+            arr, options = queue.popleft()
+
+            for i in range(len(options)):
+                next_options = options[:i] + options[i+1:]
+                new_arr = arr + [options[i]]
+
+                if next_options:
+                    queue.append((new_arr, next_options))
+                else:
+                    ans.append(new_arr)
+
+        return ans
