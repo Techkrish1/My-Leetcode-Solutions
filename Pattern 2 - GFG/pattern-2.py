ebe9031300324548c15dfcb1379240886d466742
Gokula krishnan Venkatesan <111285228+Techkrish1@users.noreply.github.com>
2023-10-15 11:09:14 +0530

    Added solution - LeetHub

--- Pattern 2 - GFG/pattern-2.py
+++ Pattern 2 - GFG/pattern-2.py
@@ -3,7 +3,9 @@
 class Solution:
     def printTriangle(self, N):
         for i in range(1,N+1):
-            print('* '*i)
+            for j in range(i):
+                print('* ', end='')
+            print()
 
 
 #{ 
